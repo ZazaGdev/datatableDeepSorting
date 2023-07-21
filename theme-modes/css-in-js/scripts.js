a71e1f0c37ae0354d3e9a1aef81fbb2e067be8ea
ZazaGdev <Gejadze250@gmail.com>
2023-07-21 23:09:34 +0400

    Added all buttons, navigation, resources

---
+++ theme-modes/css-in-js/scripts.js
@@ -0,0 +1,31 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    // Get all list items
+    let listItems = document.querySelectorAll("li[data-target]");
+    
+    let parentDivs = document.querySelectorAll("div[data-parent]");
+    parentDivs.forEach(function(div) {
+        div.style.display = "none";
+    });
+
+    document.querySelector("div[data-parent='links']").style.display = 'block';
+
+    // Attach event to each list item
+    listItems.forEach(function(listItem) {
+        listItem.addEventListener("click", function() {
+            // Get target from clicked list item
+            let target = listItem.getAttribute("data-target");
+
+            // Hide all divs
+            let divs = document.querySelectorAll("div[data-parent]");
+            divs.forEach(function(div) {
+                div.style.display = "none";
+            });
+
+            // Show matching div
+            let matchingDiv = document.querySelector('div[data-parent="' + target + '"]');
+            if (matchingDiv) {
+                matchingDiv.style.display = "block";
+            }
+        });
+    });
+});
